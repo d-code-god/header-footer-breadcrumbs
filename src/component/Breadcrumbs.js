ad3c5ea1b4ff2a47d0e0454dc73f4c51ae1e9131
C:/Program Files/Git/ dCodeGod <dcodegod@gmail.com>
2024-12-06 14:56:36 +0100

    update

--- src/component/Breadcrumbs.js
+++ src/component/Breadcrumbs.js
@@ -15,17 +15,15 @@ export const Breadcrumbs = ({ title }) => {
         style={{
           objectFit: "cover",
           backgroundBlendMode: "multiply",
-          backgroundColor: "rgba(0, 0, 0, 0.5)",
-          backgroundPosition: "center",
+          backgroundColor: "rgba(0, 0, 0, 0.9)",
           backgroundRepeat: "no-repeat",
           backgroundSize: "cover",
         }}
         src={BCImage}
         alt="Breadcrumbs Image"
-        className="bg-blend-multiply, bg-opacity-50"
       />
 
-      <div className="container flex flex-col items-center justify-center gap-4 p-20 mx-auto z-10 absolute">
+      <div className="container flex flex-col items-center justify-center gap-4 p-20 mx-auto z-10 absolute bg-black/70 h-60">
         <h1 className="text-3xl font-bold text-white">{title}</h1>
         <nav className="flex space-x-2 text-white breadcrumb">
           <Link href="/" className="breadcrumb-item">
